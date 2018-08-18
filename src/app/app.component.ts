044cb86f973cfc48ae1a3c148a3117c3f224d059
Mohan Kumar <MohanKumarYum@Gmail.com>
2018-08-18 19:00:55 +1000

    "App Works" string updated.

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "my-app",
+  // template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>App Works</h1>`
+})
+export class AppComponent {
+  name = "Angular";
+}
